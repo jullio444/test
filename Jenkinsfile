0d2afa6d22f509eeb11202c00b7ab5ebb8c4a2d4
jullio444 <julius.jjingo@gmail.com>
2021-08-04 02:46:34 -0400

    First build

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+      pipeline {
+        agent none
+        stages {
+          stage("build") {
+            agent any
+            steps {
+                sh 'mvn clean package sonar:sonar'
+            }
+          }
---
+++ src/Test.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Random number = "+Math.random());
+		System.out.println("PI ="+Math.PI);
+		System.out.println("Largest Int = "+Integer.MAX_VALUE);
+		System.out.println("Smallest Int = "+Integer.MIN_VALUE);
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter value x: ");
+		try {
+			int x = Integer.parseInt(r.readLine());
+			System.out.print("Enter value y: ");
+			int y = Integer.parseInt(r.readLine());
+			System.out.print("x+y="+(x+y));
+		} catch (NumberFormatException | IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+}
+
